862bf0729dd1684edd6908ce12000c420987e65a
April-Angela Bala <aaibala@hawaii.edu>
2017-11-09 12:05:48 -1000

    installed Bowfolios files

---
+++ app/imports/startup/client/router.js
@@ -0,0 +1,76 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { $ } from 'meteor/jquery';
+
+
+/*                        LANDING ROUTE                       */
+
+export const landingPageRouteName = 'Landing_Page';
+FlowRouter.route('/', {
+  name: landingPageRouteName,
+  action() {
+    BlazeLayout.render('Landing_Layout', { main: landingPageRouteName });
+  },
+});
+
+/*                        DIRECTORY ROUTE                       */
+
+function addDirectoryBodyClass() {
+  $('body').addClass('directory-page-body');
+}
+
+function removeDirectoryBodyClass() {
+  $('body').removeClass('directory-page-body');
+}
+
+export const directoryPageRouteName = 'Directory_Page';
+FlowRouter.route('/directory', {
+  name: directoryPageRouteName,
+  action() {
+    BlazeLayout.render('Directory_Layout', { main: directoryPageRouteName });
+  },
+  triggersEnter: [addDirectoryBodyClass],
+  triggersExit: [removeDirectoryBodyClass],
+});
+
+
+/*                        USER ROUTES                      */
+
+
+function addUserBodyClass() {
+  $('body').addClass('user-layout-body');
+}
+
+function removeUserBodyClass() {
+  $('body').removeClass('user-layout-body');
+}
+
+const userRoutes = FlowRouter.group({
+  prefix: '/:username',
+  name: 'userRoutes',
+  triggersEnter: [addUserBodyClass],
+  triggersExit: [removeUserBodyClass],
+});
+
+export const profilePageRouteName = 'Profile_Page';
+userRoutes.route('/profile', {
+  name: profilePageRouteName,
+  action() {
+    BlazeLayout.render('User_Layout', { main: profilePageRouteName });
+  },
+});
+
+export const filterPageRouteName = 'Filter_Page';
+userRoutes.route('/filter', {
+  name: filterPageRouteName,
+  action() {
+    BlazeLayout.render('User_Layout', { main: filterPageRouteName });
+  },
+});
+
+/*                        MISC ROUTES                       */
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('Page_Not_Found');
+  },
+};
