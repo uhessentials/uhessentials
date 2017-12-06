1df206fc4ede9b682d935e90c6581483cb715c90
Creighton Gorai <CGorai@hawaii.edu>
2017-12-06 10:34:27 -1000

    changing homepage routing

--- app/imports/ui/pages/thread/events-thread.js
+++ app/imports/ui/pages/thread/events-thread.js
@@ -1,8 +1,68 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { _ } from 'meteor/underscore';
+import { Profiles } from '/imports/api/profile/ProfileCollection';
 
-Template.User_Header.helpers({
+const displaySuccessMessage = 'displaySuccessMessage';
+const displayErrorMessages = 'displayErrorMessages';
+
+Template.Events_Thread.onCreated(function onCreated() {
+  this.subscribe(Profiles.getPublicationName());
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(displaySuccessMessage, false);
+  this.messageFlags.set(displayErrorMessages, false);
+  this.context = Profiles.getSchema().namedContext('Events_Thread');
+});
+
+Template.Events_Thread.helpers({
+  successClass() {
+    return Template.instance().messageFlags.get(displaySuccessMessage) ? 'success' : '';
+  },
+  displaySuccessMessage() {
+    return Template.instance().messageFlags.get(displaySuccessMessage);
+  },
+  errorClass() {
+    return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
+  },
+  profile() {
+    return Profiles.findDoc(FlowRouter.getParam('username'));
+  },
   routeUserName() {
     return FlowRouter.getParam('username');
   },
 });
+
+Template.Events_Thread.events({
+  'submit .home-data-form'(event, instance) {
+    event.preventDefault();
+    const firstName = event.target.First.value;
+    const lastName = event.target.Last.value;
+    const title = event.target.Title.value;
+    const username = FlowRouter.getParam('username'); // schema requires username.
+    const picture = event.target.Picture.value;
+    const bio = event.target.Bio.value;
+
+    const updatedHomeData = { firstName, lastName, title, picture, bio, username };
+
+    // Clear out any old validation errors.
+    instance.context.reset();
+    // Invoke clean so that updatedProfileData reflects what will be inserted.
+    const cleanData = Profiles.getSchema().clean(updatedHomeData);
+    // Determine validity.
+    instance.context.validate(cleanData);
+
+    if (instance.context.isValid()) {
+      const docID = Profiles.findDoc(FlowRouter.getParam('username'))._id;
+      const id = Profiles.update(docID, { $set: cleanData });
+      instance.messageFlags.set(displaySuccessMessage, id);
+      instance.messageFlags.set(displayErrorMessages, false);
+    } else {
+      instance.messageFlags.set(displaySuccessMessage, false);
+      instance.messageFlags.set(displayErrorMessages, true);
+    }
+  },
+
+
+});
+
