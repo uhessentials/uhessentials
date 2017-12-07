d90cfe714e1cab90b21ed49dfeb280dcc940d9be
April-Angela Bala <aaibala@hawaii.edu>
2017-12-07 11:32:37 -1000

    fixed small error from merge

--- app/imports/ui/components/user/index.js
+++ app/imports/ui/components/user/index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import './threads-form-field.html';
 import './threads-form-field.js';
 
 import './campus-form-field.html';
 import './campus-form-field.js';
-=======
-import './thread-form-field.html';
-import './thread-form-field.js';
->>>>>>> master
