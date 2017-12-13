10ad27906f0c686a808b17777bcffc7d3f3bf01c
April-Angela Bala <aaibala@hawaii.edu>
2017-12-13 12:32:14 -1000

    resolving issue

--- app/imports/ui/pages/topic/index.js
+++ app/imports/ui/pages/topic/index.js
@@ -2,8 +2,4 @@ import './topic-page.html';
 import './topic-page.css';
 import './topic-page.js';
 
-import './commute-thread-topics';
-import './degree-thread-topics';
-import './events-thread-topics';
 import './food-thread-topics';
-import './security-thread-topics';
